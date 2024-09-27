83cfd43d539183979a21aa197514dd8c1b49f592
Tushar Rajput <tr262105@gmail.com>
2024-09-27 22:21:39 +0530

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-function navigationToSecond() {
-    const username = document.getElementById("username").value;
-    if (username) {
-        window.location.href = `pop.html?name=${encodeURIComponent(username)}`;
-    } else {
-        alert("Please enter your name.");
-    }
-}
